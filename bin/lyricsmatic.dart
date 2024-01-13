c7d8e06c3a4681675a587320c9185d6d2236dab9
Yellowsink <yellowsink@riseup.net>
2024-01-13 22:49:40 +0000

    full of errors cause of constructors but im tired and gonna call it a night

--- bin/lyricsmatic.dart
+++ bin/lyricsmatic.dart
@@ -2,9 +2,8 @@ import 'dart:io';
 
 import 'package:lyricsmatic/lyricsmatic.dart' as lyricsmatic;
 
-void main(List<String> arguments) {
-  final tags = lyricsmatic
-      .getFileTags(File("~/Music/brakence/hypochondriac/04. teeth.opus"));
-
-  print(tags);
+void main(List<String> arguments) async {
+  final file = File("/home/sink/Music/brakence/hypochondriac/04. teeth.opus");
+  final songMeta = await lyricsmatic.SongMeta.fromFile(file);
+  print(songMeta.isrc);
 }
--- lib/lyricsmatic.dart
+++ lib/lyricsmatic.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart';
 // someone is going to murder me for using regex and processes for this.
 Future<Map<String, String>> getFileTags(File f) async {
   // run ffprobe
-  var res = await Process.run("ffprobe", [
+  final res = await Process.run("ffprobe", [
     "-of",
     "json=c=1",
     "-show_entries",
@@ -18,34 +18,80 @@ Future<Map<String, String>> getFileTags(File f) async {
     f.absolute.path
   ]);
 
-  var parsed = jsonDecode(res.stdout);
+  final parsed = jsonDecode(res.stdout);
 
-  return (parsed["format"] as Map).containsKey("tags")
+  final Map<String, dynamic> tags = parsed["format"]!["tags"] != null
       ? parsed["format"]["tags"]
       : parsed["streams"][0]["tags"];
+
+  return tags
+      .map((key, value) => MapEntry(key, (value as String).toUpperCase()));
 }
 
 class SongMeta {
   File fs;
-  // preferred
-  String? isrc; // International Standard Recording Code
-  // second, third choice, etc.
+  // search options to try, in order of preference
+  // if we have the ISRC we don't need to search
+  String isrc; // International Standard Recording Code
   String? mbidTrack; // Musicbrainz Track ID
-  String? mbidRecording;
+  String? mbidRecording; // Musicbrainz Recording ID
   // and failing that (we probably have all or none), search:
   String? trackName;
   String? artistName;
 
-  SongMeta(this.fs) {
-    throw UnimplementedError();
+  // async construction is cool! :(
+  SongMeta._(this.fs, this.isrc, this.mbidTrack, this.mbidRecording,
+      this.trackName, this.artistName);
+
+  static Future<SongMeta> fromFile(File fs) async {
+    final tags = await getFileTags(fs);
+    return SongMeta._(fs, tags["ISRC"]!, tags["MUSICBRAINZ_RELEASETRACKID"],
+        tags["MUSICBRAINZ_TRACKID"], tags["TITLE"], tags["ARTIST"]);
   }
+}
+
+class LyricsLine {
+  double start;
+  double end;
+  //String key; // TODO: what is itunes:key?
+  int singerId;
+  List<(double start, double end, String txt, bool emphasis)> chunks;
+}
+
+class LyricsBlock {
+  double start;
+  double end;
+  String? songPart;
+  List<LyricsLine> lines;
+}
+
+class Lyrics {
+  List<(String type, String person)> credits;
+  List<String> singers;
+  double leadingSilence;
+  double duration;
+  List<LyricsBlock> blocks;
 }
 
 class LyricsFetcher {
   final Client client = Client();
-  List<SongMeta> queue = [];
 
   void close() {
     client.close();
+  }
+
+  Future getLyrics(SongMeta song) async {
+    // TODO: implement search for songs without ISRCs
+    final uri = Uri(
+        scheme: "https",
+        host: "beautiful-lyrics.socalifornian.live",
+        pathSegments: ["lyrics", song.isrc]);
+
+    final res = await client.get(uri);
+    assert(res.statusCode == 200);
+    final json = jsonDecode(res.body);
+    assert(json["Source"] == "AppleMusic");
+
+    final String ttml = json["Content"];
   }
 }
